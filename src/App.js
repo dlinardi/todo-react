b0c0a1241defd42286b44ba2dd51f8c823ff396b
dlinardi <dlinardi98@gmail.com>
2021-02-03 21:33:38 -0500

    constructor + render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
+export default class App extends Component {
+  // prepare data before app starts so render can access that data
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: "Name1",
+    };
+  }
+
+  render = () => (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="bg-primary text-white text-center p2">Todo List</h2>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
